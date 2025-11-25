18cd4afe0257e14231ae6d83420b681e76943b1d
wellckome <wellckome@gmail.com>
2025-11-25 18:52:06 -0300

    Atualiza├º├úo autom├ítica

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const input   = document.getElementById('numberInput');
 const result  = document.getElementById('result');
-
-
 const themeSelect = document.getElementById('themeSelect');
-
-themeSelect.addEventListener('change', () => {
-  const selectedTheme = themeSelect.value;
-  document.body.className = selectedTheme;
-});
-
 
 // Função de pesquisa
 function pesquisar() {
@@ -31,25 +23,4 @@ function pesquisar() {
 function limpar() {
   input.value = '';
   input.focus();
-  result.textContent = 'Digite um número e pressione Enter';
-  result.classList.remove("valid", "invalid");
-}
-
-// Evento Enter
-input.addEventListener('keydown', e => {
-  if (e.key === 'Enter') {
-    pesquisar();
-  }
-});
-
-// Função para trocar tema
-function setTheme(theme) {
-  document.body.className = theme;
-}
-
-// Botões de tema
-document.getElementById('btnDark').onclick   = () => setTheme('dark');
-document.getElementById('btnLight').onclick  = () => setTheme('light');
-document.getElementById('btnBlue').onclick   = () => setTheme('blue');
-document.getElementById('btnPurple').onclick = () => setTheme('purple');
-document.getElementById('btnGold').onclick   = () => setTheme('gold');
+  result.textContent

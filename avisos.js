d4e9e515a73b19fdd18343950f2d14b0a1c26c4f
wellckome <wellckome@gmail.com>
2025-11-27 07:45:45 -0300

    Atualiza├º├úo autom├ítica

--- avisos.js
+++ avisos.js
@@ -1,24 +1,24 @@
 // Avisos separados em constantes
 
 export const avisosNumero = `
-<p>⚡ Na pesquisa por número, você vai digitar o número correspondente à sua Seed-Phrase para restaurar sua carteira.</p>
-<p>⚡ Supondo que já tenha anotado em um lugar seguro seus números correspondentes.</p>
-<p>⚡ Ou seja, na prática você pegará seus números e restaurará sua Seed-Phrase (Wallet).</p>
-<p>⚡ Mas lembre-se que o ideal é ter sempre uma Hard Wallet com uma PassPhrase segura e complexa.</p>
-<p>⚡ PassPhrase você só terá em Cold Wallet ou Air Gaps.</p>
-<p>⚡ Caso não tenha os números, use a pesquisa por palavras e crie sua lista.</p>
-<p>⚡ Lembrando que sua pesquisa não será armazenada em nenhum lugar, apenas no seu caderno.</p>
-<p>⚡ Boa prática: sempre pesquisar mais palavras do que precisa (ex: se for 12 → pesquise 15, se for 24 → pesquise 30).</p>
+<p style="color:#00ff7f;">👉 Na pesquisa por número, você vai digitar o número correspondente à sua Seed-Phrase para restaurar sua carteira.</p>
+<p style="color:#00ff7f;">👉 Supondo que já tenha anotado em um lugar seguro seus números correspondentes.</p>
+<p style="color:#00ff7f;">👉 Ou seja, na prática você pegará seus números e restaurará sua Seed-Phrase (Wallet).</p>
+<p style="color:#00ff7f;">👉 Mas lembre-se que o ideal é ter sempre uma Hard Wallet com uma PassPhrase segura e complexa.</p>
+<p style="color:#00ff7f;">👉 PassPhrase você só terá em Cold Wallet ou Air Gaps.</p>
+<p style="color:#00ff7f;">👉 Caso não tenha os números, use a pesquisa por palavras e crie sua lista.</p>
+<p style="color:#00ff7f;">👉 Lembrando que sua pesquisa não será armazenada em nenhum lugar, apenas no seu caderno.</p>
+<p style="color:#00ff7f;">👉 Boa prática: sempre pesquisar mais palavras do que precisa (ex: se for 12 → pesquise 15, se for 24 → pesquise 30).</p>
 `;
 
 export const avisosPalavra = `
-<p>⚡ Na pesquisa por palavra, você vai digitar a palavra correspondente à sua Seed-Phrase para fazer backup dos números.</p>
-<p>⚡ Ou seja, na prática você guardará uma série de números que não te dá correspondência direta com Seed-Phrase.</p>
-<p>⚡ Mas lembre-se que o ideal é ter sempre uma Hard Wallet com uma PassPhrase segura e complexa.</p>
-<p>⚡ PassPhrase significa uma camada extra de segurança além da Seed-Phrase.</p>
-<p>⚡ A PassPhrase é escolhida por você e não consta na lista BIP39.</p>
-<p>⚡ Exemplos: time do coração, uma frase ou qualquer palavra — desde que nunca esqueça.</p>
-<p>⚡ Uma PassPhrase pode conter até 100 caracteres. Quanto mais longa e complexa, melhor.</p>
-<p>⚡ Lembrando que sua pesquisa não será armazenada em nenhum lugar, apenas no seu caderno.</p>
-<p>⚡ Boa prática: sempre pesquisar mais palavras do que precisa (ex: se for 12 → pesquise 15, se for 24 → pesquise 30).</p>
+<p style="color:#00ff7f;">👉Na pesquisa por palavra, você vai digitar a palavra correspondente à sua Seed-Phrase para fazer backup dos números.</p>
+<p style="color:#00ff7f;">👉 Ou seja, na prática você guardará uma série de números que não te dá correspondência direta com Seed-Phrase.</p>
+<p style="color:#00ff7f;">👉 Mas lembre-se que o ideal é ter sempre uma Hard Wallet com uma PassPhrase segura e complexa.</p>
+<p style="color:#00ff7f;">👉 PassPhrase significa uma camada extra de segurança além da Seed-Phrase.</p>
+<p style="color:#00ff7f;">👉 A PassPhrase é escolhida por você e não consta na lista BIP39.</p>
+<p style="color:#00ff7f;">👉 Exemplos: time do coração, uma frase ou qualquer palavra — desde que nunca esqueça.</p>
+<p style="color:#00ff7f;">👉 Uma PassPhrase pode conter até 100 caracteres. Quanto mais longa e complexa, melhor.</p>
+<p style="color:#00ff7f;">👉 Lembrando que sua pesquisa não será armazenada em nenhum lugar, apenas no seu caderno.</p>
+<p style="color:#00ff7f;">👉 Boa prática: sempre pesquisar mais palavras do que precisa (ex: se for 12 → pesquise 15, se for 24 → pesquise 30).</p>
 `;

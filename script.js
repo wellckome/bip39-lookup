95428d1cefa68b8233351a4db862cd66e27848e2
wellckome <wellckome@gmail.com>
2025-11-27 13:51:55 -0300

    Atualiza├º├úo autom├ítica

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function mostrarAviso(mensagem, duracaoSegundos) {
       avisoIntervalo = null;
       obs.style.display = 'none';
       contador.style.display = 'none';
-      container.style.maxWidth = '500px';
+      container.style.maxWidth = '850px';
       return;
     }
     contador.textContent = `⏳ ${tempoRestante} s...`;
